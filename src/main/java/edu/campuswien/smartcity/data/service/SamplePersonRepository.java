487c33f4958b98b6de339e33a15e5582505439e8
nrnazifi <navid.nazifi@gmail.com>
2022-02-27 13:46:24 +0100

    remove useless classes and files

--- src/main/java/edu/campuswien/smartcity/data/service/SamplePersonRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.campuswien.smartcity.data.service;
-
-import edu.campuswien.smartcity.data.entity.SamplePerson;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
-
-}
