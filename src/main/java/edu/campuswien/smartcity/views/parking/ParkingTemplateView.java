5640ef5f9012cf1d201c75775c44ba559488ab4c
nrnazifi <navid.nazifi@gmail.com>
2022-03-02 11:14:37 +0100

    add buttons on top an redirect to add-form

--- src/main/java/edu/campuswien/smartcity/views/parking/ParkingTemplateView.java
+++ src/main/java/edu/campuswien/smartcity/views/parking/ParkingTemplateView.java
@@ -3,14 +3,13 @@ package edu.campuswien.smartcity.views.parking;
 import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.littemplate.LitTemplate;
-import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -29,22 +28,28 @@ import java.util.List;
 public class ParkingTemplateView extends LitTemplate implements HasComponents, HasStyle {
     private ParkingLotService parkingLotService;
 
-    @Id("import")
+    @Id("btnAdd")
+    private Button btnAdd;
+
+    @Id("btnImport")
     private Button btnImport;
 
     public ParkingTemplateView(@Autowired ParkingLotService parkingLotService) {
         this.parkingLotService = parkingLotService;
 
         addClassNames("parking-template-view", "flex", "flex-col", "h-full");
-//        import = new Button("Left", new Icon(VaadinIcon.ARROW_LEFT));
-//        import.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_SUCCESS);
+        btnAdd.setIcon(new Icon(VaadinIcon.PLUS));
+        btnImport.setIcon(new Icon(VaadinIcon.UPLOAD));
+        btnAdd.addClickListener(e -> {
+            UI.getCurrent().navigate(ParkingFormView.class);
+        });
         btnImport.addClickListener(e -> {
-           System.out.println("ddd");
+           //Import temp
         });
 
 
-        List<ParkingLot> parkinglots = parkingLotService.list();
-        for (ParkingLot lot : parkinglots) {
+        List<ParkingLot> parkingLots = parkingLotService.list();
+        for (ParkingLot lot : parkingLots) {
             add(new ParkingTemplateViewCard(lot));
         }
     }
